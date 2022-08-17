383af3ccb6d9fe87aa3e8562460971b9aa5684ec
lowtierre <lorenzoterreni1997@gmail.com>
2022-08-17 22:17:23 +0200

    Ho manipolato il DOM: modal dinamico

--- test.js
+++ test.js
@@ -1,180 +1,6 @@
 const body = document.querySelector('body')
 
-// Buttons declaration
-
-const yepA = document.getElementById("yepa");
-const yepB = document.getElementById("yepb");
-const yepC = document.getElementById("yepc");
-const yepD = document.getElementById("yepd");
-const nopA = document.getElementById("nopa");
-const nopB = document.getElementById("nopb");
-const nopC = document.getElementById("nopc");
-const nopD = document.getElementById("nopd");
-
-// Buttons event
-
-yepA.addEventListener("click", function() {
-    yepA.classList.toggle("btOn")
-    nopA.classList.remove("btOn")
-});
-
-yepB.addEventListener("click", function() {
-    yepB.classList.toggle("btOn")
-    nopB.classList.remove("btOn")
-});
-
-yepC.addEventListener("click", function() {
-    yepC.classList.toggle("btOn")
-    nopC.classList.remove("btOn")
-});
-
-yepD.addEventListener("click", function() {
-    yepD.classList.toggle("btOn")
-    nopD.classList.remove("btOn")
-});
-
-nopA.addEventListener("click", function() {
-    nopA.classList.toggle("btOn")
-    yepA.classList.remove("btOn")
-});
-
-nopB.addEventListener("click", function() {
-    nopB.classList.toggle("btOn")
-    yepB.classList.remove("btOn")
-});
-
-nopC.addEventListener("click", function() {
-    nopC.classList.toggle("btOn")
-    yepC.classList.remove("btOn")
-});
-
-nopD.addEventListener("click", function() {
-    nopD.classList.toggle("btOn")
-    yepD.classList.remove("btOn")
-});
-
-// Beer Panel
-
-function createPanel() {
-  const panel = document.createElement("div");
-  body.appendChild(panel);
-  panel.setAttribute('class', 'beer');
-  
-  createTable()
-}
-
-function createTable() {
-  const table1 = document.createElement("table");
-  table1.setAttribute('class', 'beer-table1');
-  const table2 = document.createElement("table");
-  table2.setAttribute('class', 'beer-table2');
-  var tables = [table1, table2];
-  for (i=0; i<tables.length; i++) {
-    const tr1 = document.createElement('tr');
-    const tr2 = document.createElement('tr');
-    tables[i].appendChild(tr1)
-    tables[i].appendChild(tr2)
-    const th1 = document.createElement('th');
-    const th2 = document.createElement('th');
-    tr1.appendChild(th1);
-    tr1.appendChild(th2)
-    const td1 = document.createElement('td');
-    const td2 = document.createElement('td');
-    tr2.appendChild(td1)
-    tr2.appendChild(td2)
-    }
-  
-
-
-  const bImg = document.createElement("div");
-  bImg.setAttribute('class', 'beer-image');
-  const bDescr = document.createElement("div");
-  bDescr.setAttribute('class', 'beer-description');
-  const bName = document.createElement("p");
-  bName.setAttribute('class', 'beer-name');
-  const btype = document.createElement("p");
-  btype.setAttribute('class', 'typo');
-  const btaste = document.createElement("p");
-  btaste.setAttribute('class', 'taste');
-  const bcolor = document.createElement("p");
-  bcolor.setAttribute('class', 'color');
-  const bgrad = document.createElement("p");
-  bgrad.setAttribute('class', 'grad');
-}
-
-const result = document.querySelector(".result");
-const close = document.querySelector(".close");
-// const panel = document.querySelector(".beer");
-const blanket = document.querySelector(".blanket");
-// const bName = document.querySelector(".beer-name")
-// const btype = document.querySelector(".typo")
-// const btaste = document.querySelector(".taste")
-// const bcolor = document.querySelector(".color")
-// const bgrad = document.querySelector(".grad")
-// const bImg = document.querySelector(".beer-img")
-const gancio = document.querySelector(".inizio")
-
-// button control
-
-let arr1 = [null,null,null,null]
-const buttonY = [yepA, yepB, yepC, yepD]
-const buttonN = [nopA, nopB, nopC, nopD]
-
-function answerY(x,y) {
-    for (i=0; i<(x.length); i++) {
-        if (x[i].classList.contains("btOn")) {
-            y[i] = true;
-        }
-    }
-    console.log(y)
-}
-
-function answerN(x,y) {
-    for (i=0; i<(x.length); i++) {
-        if (x[i].classList.contains("btOn")) {
-            y[i] = false;
-        }
-    }
-    console.log(y)
-}
-
-// result button
-
-result.addEventListener("click", function() {
-  createPanel()
-  // panel.style.display="block";
-  blanket.style.display="block";
-  window.scrollTo(0, 720);
-  answerY(x = buttonY, y = arr1);
-  answerN(x = buttonN, y = arr1);
-  insert(arr1);
-});
-
-// close modal
-
-// close.addEventListener("click", function() {
-//     yepA.classList.remove("btOn")
-//     yepB.classList.remove("btOn")
-//     yepC.classList.remove("btOn")
-//     yepD.classList.remove("btOn")
-//     nopA.classList.remove("btOn")
-//     nopB.classList.remove("btOn")
-//     nopC.classList.remove("btOn")
-//     nopD.classList.remove("btOn")
-//     arr1[0] = null
-//     arr1[1] = null
-//     arr1[2] = null
-//     arr1[3] = null
-//     panel.style.display="none";
-//     blanket.style.display="none";
-//     window.scrollTo({
-//       top: 720,
-//       left: 0,
-//       behavior: "smooth"
-//     });
-// });
-
-// birre list
+// beer list 
 
 var birre = [
   {
@@ -307,58 +133,268 @@ var birre = [
   },
 ]
 
-// fill modal with the right beer
+// Buttons declaration
 
-function fillModal(index) {
+const yepA = document.getElementById("yepa");
+const yepB = document.getElementById("yepb");
+const yepC = document.getElementById("yepc");
+const yepD = document.getElementById("yepd");
+const nopA = document.getElementById("nopa");
+const nopB = document.getElementById("nopb");
+const nopC = document.getElementById("nopc");
+const nopD = document.getElementById("nopd");
+
+// Buttons event
+
+yepA.addEventListener("click", function() {
+    yepA.classList.toggle("btOn")
+    nopA.classList.remove("btOn")
+});
+
+yepB.addEventListener("click", function() {
+    yepB.classList.toggle("btOn")
+    nopB.classList.remove("btOn")
+});
+
+yepC.addEventListener("click", function() {
+    yepC.classList.toggle("btOn")
+    nopC.classList.remove("btOn")
+});
+
+yepD.addEventListener("click", function() {
+    yepD.classList.toggle("btOn")
+    nopD.classList.remove("btOn")
+});
+
+nopA.addEventListener("click", function() {
+    nopA.classList.toggle("btOn")
+    yepA.classList.remove("btOn")
+});
+
+nopB.addEventListener("click", function() {
+    nopB.classList.toggle("btOn")
+    yepB.classList.remove("btOn")
+});
+
+nopC.addEventListener("click", function() {
+    nopC.classList.toggle("btOn")
+    yepC.classList.remove("btOn")
+});
+
+nopD.addEventListener("click", function() {
+    nopD.classList.toggle("btOn")
+    yepD.classList.remove("btOn")
+});
+
+// Beer Panel
+
+function createTable() {
+
+}
+
+function createPanel(index) {
+  
+  const panel = document.createElement("div");
+  body.appendChild(panel);
+  panel.setAttribute('class', 'beer');
+
+  const bImg = document.createElement("div");
+  bImg.setAttribute('class', 'beer-img');
+  panel.appendChild(bImg)
+
+  const bName = document.createElement("p");
+  bName.setAttribute('class', 'beer-name');
+  panel.appendChild(bName)
+
+  const bDescr = document.createElement("div");
+  bDescr.setAttribute('class', 'beer-description');
+  panel.appendChild(bDescr)
+
+  //table one
+  const table1 = document.createElement("table");
+  table1.setAttribute('class', 'beer-table1');
+  const tr1 = document.createElement('tr');
+  const tr2 = document.createElement('tr');
+  table1.appendChild(tr1)
+  table1.appendChild(tr2)
+  const th1 = document.createElement('th');
+  const th2 = document.createElement('th');
+  const div1 = document.createElement('div');
+  div1.setAttribute('class', 'property');
+  th1.appendChild(div1);
+  const div2 = document.createElement('div');
+  div2.setAttribute('class', 'property');
+  th2.appendChild(div2);
+  tr1.appendChild(th1);
+  tr1.appendChild(th2);
+  const td1 = document.createElement('td');
+  const td2 = document.createElement('td');
+  const btype = document.createElement("p");
+  btype.setAttribute('class', 'typo');
+  td1.appendChild(btype);
+  const btaste = document.createElement("p");
+  btaste.setAttribute('class', 'taste');
+  td2.appendChild(btaste);
+  tr2.appendChild(td1);
+  tr2.appendChild(td2);
+
+  //table two
+  const table2 = document.createElement("table");
+  table2.setAttribute('class', 'beer-table2');
+  const tr3 = document.createElement('tr');
+  const tr4 = document.createElement('tr');
+  table2.appendChild(tr3)
+  table2.appendChild(tr4)
+  const th3 = document.createElement('th');
+  const th4 = document.createElement('th');
+  const div3 = document.createElement('div');
+  div3.setAttribute('class', 'property');
+  th3.appendChild(div3);
+  const div4 = document.createElement('div');
+  div4.setAttribute('class', 'property');
+  th4.appendChild(div4);
+  tr3.appendChild(th3);
+  tr3.appendChild(th4);
+  const td3 = document.createElement('td');
+  const td4 = document.createElement('td');
+  const bcolor = document.createElement("p");
+  bcolor.setAttribute('class', 'color');
+  td3.appendChild(bcolor);
+  const bgrad = document.createElement("p");
+  bgrad.setAttribute('class', 'grad');
+  td4.appendChild(bgrad);
+  tr4.appendChild(td3);
+  tr4.appendChild(td4);
+
+  bDescr.appendChild(table1);
+  bDescr.appendChild(table2);
+  
+  //fill modal
+
+  bImg.style.backgroundImage = birre[index].immagine
   bName.innerHTML = birre[index].nome
   btype.innerHTML = birre[index].tipo
   btaste.innerHTML = birre[index].gusto
   bcolor.innerHTML = birre[index].colore
   bgrad.innerHTML = birre[index].gradazione
-  bImg.style.backgroundImage = birre[index].immagine
 }
+
+// button control
+
+let arr1 = [null,null,null,null]
+const buttonY = [yepA, yepB, yepC, yepD]
+const buttonN = [nopA, nopB, nopC, nopD]
+
+function answerY(x,y) {
+    for (i=0; i<(x.length); i++) {
+        if (x[i].classList.contains("btOn")) {
+            y[i] = true;
+        }
+    }
+    console.log(y)
+}
+
+function answerN(x,y) {
+    for (i=0; i<(x.length); i++) {
+        if (x[i].classList.contains("btOn")) {
+            y[i] = false;
+        }
+    }
+    console.log(y)
+}
+
+
+// fill modal with the right beer
 
 function insert(arr1) {
 
   if (arr1[0] == true && arr1[1] == true && arr1[2] == true && arr1[3] == true) { 
-        fillModal(0)
+        createPanel(0)
       } else if (arr1[0] == true && arr1[1] == true && arr1[2] == true && arr1[3] == false) { 
-        fillModal(1)
+        createPanel(1)
       } else if (arr1[0] == true && arr1[1] == true && arr1[2] == false && arr1[3] == true) { 
-        fillModal(2)
+        createPanel(2)
       } else if (arr1[0] == true && arr1[1] == false && arr1[2] == true && arr1[3] == true) { 
-        fillModal(3)
+        createPanel(3)
       } else if (arr1[0] == true && arr1[1] == true && arr1[2] == false && arr1[3] == false) { 
-        fillModal(4)
+        createPanel(4)
       } else if (arr1[0] == true && arr1[1] == false && arr1[2] == true && arr1[3] == false) { 
-        fillModal(5)
+        createPanel(5)
       } else if (arr1[0] == true && arr1[1] == false && arr1[2] == false && arr1[3] == true) { 
-        fillModal(6)
+        createPanel(6)
       } else if (arr1[0] == true && arr1[1] == false && arr1[2] == false && arr1[3] == false) { 
-        fillModal(7)
+        createPanel(7)
       } else if (arr1[0] == false && arr1[1] == false && arr1[2] == false && arr1[3] == false) { 
-        fillModal(8)
+        createPanel(8)
       } else if (arr1[0] == false && arr1[1] == false && arr1[2] == false && arr1[3] == true) { 
-        fillModal(9)
+        createPanel(9)
       } else if (arr1[0] == false && arr1[1] == false && arr1[2] == true && arr1[3] == false) { 
-        fillModal(10)
+        createPanel(10)
       } else if (arr1[0] == false && arr1[1] == true && arr1[2] == false && arr1[3] == false) { 
-        fillModal(11)
+        createPanel(11)
       } else if (arr1[0] == false && arr1[1] == false && arr1[2] == true && arr1[3] == true) { 
-        fillModal(12)
+        createPanel(12)
       } else if (arr1[0] == false && arr1[1] == true && arr1[2] == true && arr1[3] == false) { 
-        fillModal(13)
+        createPanel(13)
       } else if (arr1[0] == false && arr1[1] == true && arr1[2] == false && arr1[3] == true) { 
-        fillModal(14)
+        createPanel(14)
       } else if (arr1[0] == false && arr1[1] == true && arr1[2] == true && arr1[3] == true) { 
-        fillModal(15)
-      } else if (arr1[0] == null || arr1[1] == null || arr1[2] == null || arr1[3] == null) {
-      bName.style.fontSize = "1.2em";
-      bName.innerHTML = "Devi rispondere a tutte le domande se vuoi sorseggiare la birra che fa per te";
-      btype.innerHTML = "n/d";
-      btaste.innerHTML = "n/d";
-      bcolor.innerHTML = "n/d";
-      bgrad.innerHTML = "n/d";
-      bImg.style.backgroundImage = "url(img/Birre/Birra_vuota.png)";
-      }
+        createPanel(15)
+      } //else if (arr1[0] == null || arr1[1] == null || arr1[2] == null || arr1[3] == null) {
+      // bName.style.fontSize = "1.2em";
+      // bName.innerHTML = "Devi rispondere a tutte le domande se vuoi sorseggiare la birra che fa per te";
+      // btype.innerHTML = "n/d";
+      // btaste.innerHTML = "n/d";
+      // bcolor.innerHTML = "n/d";
+      // bgrad.innerHTML = "n/d";
+      // bImg.style.backgroundImage = "url(img/Birre/Birra_vuota.png)";
+      // }
 }
+
+const result = document.querySelector(".result");
+const close = document.querySelector(".close");
+// const panel = document.querySelector(".beer");
+const blanket = document.querySelector(".blanket");
+// const bName = document.querySelector(".beer-name")
+// const btype = document.querySelector(".typo")
+// const btaste = document.querySelector(".taste")
+// const bcolor = document.querySelector(".color")
+// const bgrad = document.querySelector(".grad")
+// const bImg = document.querySelector(".beer-img")
+const gancio = document.querySelector(".inizio")
+
+// result button
+
+result.addEventListener("click", function() {
+  // panel.style.display="block";
+  blanket.style.display="block";
+  window.scrollTo(0, 720);
+  answerY(x = buttonY, y = arr1);
+  answerN(x = buttonN, y = arr1);
+  insert(arr1);
+});
+
+// close modal
+
+// close.addEventListener("click", function() {
+//     yepA.classList.remove("btOn")
+//     yepB.classList.remove("btOn")
+//     yepC.classList.remove("btOn")
+//     yepD.classList.remove("btOn")
+//     nopA.classList.remove("btOn")
+//     nopB.classList.remove("btOn")
+//     nopC.classList.remove("btOn")
+//     nopD.classList.remove("btOn")
+//     arr1[0] = null
+//     arr1[1] = null
+//     arr1[2] = null
+//     arr1[3] = null
+//     panel.style.display="none";
+//     blanket.style.display="none";
+//     window.scrollTo({
+//       top: 720,
+//       left: 0,
+//       behavior: "smooth"
+//     });
+// });
+
