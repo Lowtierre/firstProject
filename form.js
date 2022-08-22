7cce749a5cd47f0dfabc1e6c9f0ac40e765ca08f
lowtierre <lorenzoterreni1997@gmail.com>
2022-08-22 19:05:31 +0200

    Change access page

--- form.js
+++ form.js
@@ -12,24 +12,46 @@ enter.addEventListener("click", function() {
     var month = parseInt(mm.value)
     var year = parseInt(yyyy.value)
 
+    var now = new Date();
+    var nowDay = now.getDate();
+    var nowMonth = now.getMonth();
+    var nowYear = now.getFullYear();
+
     if ((day>0 && day<32) && (month>0 && month<13) && (year>0 && year<2023) && yyyy.value.length==4) {
-        formDiv.style.visibility = "hidden"
-        window.scrollTo(top)
+        dd.classList.remove("error");
+        mm.classList.remove("error");
+        yyyy.classList.remove("error");
+        if((nowYear - year)>18) {
+            formDiv.style.visibility = "hidden"
+            window.scrollTo(top)
+        }
+        else if ((nowYear - year)==18){
+            if((nowMonth + 1)>month) {
+                formDiv.style.visibility = "hidden"
+                window.scrollTo(top)
+            }
+            else if ((nowMonth + 1) == month) {
+                if (nowDay>day || nowDay==day) {
+                    formDiv.style.visibility = "hidden"
+                    window.scrollTo(top)
+                } else {spanError.innerHTML = "sei piccino"}
+            } else {spanError.innerHTML = "sei piccino"}
+        } else {spanError.innerHTML = "sei piccino"}
     } else if (!(day>0 && day<32)) {
-        console.log("bad day")
+        mm.classList.remove("error");
+        yyyy.classList.remove("error");
         dd.classList.add("error");
-        var badDay = document.createTextNode("invalid day value");
-        spanError.appendChild(badDay)
+        spanError.innerHTML = "invalid day value"
     } else if (!(month>0 && month<13)) {
-        console.log("bad month")
+        dd.classList.remove("error");
+        yyyy.classList.remove("error");
         mm.classList.add("error")
-        var badMonth = document.createTextNode("invalid month value");
-        spanError.appendChild(badMonth)
+        spanError.innerHTML = "invalid month value"
     } else if (!(year>0 && year<2023) || yyyy.value.length!=4) {
-        console.log("bad year")
-        yy.classList.add("error")
-        var badYear = document.createTextNode("invalid year value");
-        spanError.appendChild(badYear)
+        dd.classList.remove("error");
+        mm.classList.remove("error");
+        yyyy.classList.add("error")
+        spanError.innerHTML = "invalid year value"
     }
     
 })
