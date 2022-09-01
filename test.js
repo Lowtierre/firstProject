74d2013deeec6dd661f6c47af9b40115e69c6192
lowtierre <lorenzoterreni1997@gmail.com>
2022-09-01 15:52:24 +0200

    Try & Catch

--- test.js
+++ test.js
@@ -196,131 +196,131 @@ nopD.addEventListener("click", function() {
 
 // Beer Panel
 
-function createTable() {
-
-}
-
 function createPanel(index) {
 
-  const panel = document.createElement("div");
-  panel.setAttribute('class', 'beer');
-  body.appendChild(panel);
+  try{
+    const panel = document.createElement("div");
+    panel.setAttribute('class', 'beer');
+    body.appendChild(panel);
 
-  const close = document.createElement("button");
-  close.setAttribute('class', 'close');
-  panel.appendChild(close);
+    const close = document.createElement("button");
+    close.setAttribute('class', 'close');
+    panel.appendChild(close);
 
-  const ics = document.createElement("i");
-  ics.setAttribute('class', 'fa-solid fa-xmark');
-  close.appendChild(ics)
+    const ics = document.createElement("i");
+    ics.setAttribute('class', 'fa-solid fa-xmark');
+    close.appendChild(ics)
 
-  const bImg = document.createElement("div");
-  bImg.setAttribute('class', 'beer-img');
-  panel.appendChild(bImg)
+    const bImg = document.createElement("div");
+    bImg.setAttribute('class', 'beer-img');
+    panel.appendChild(bImg)
 
-  const bName = document.createElement("p");
-  bName.setAttribute('class', 'beer-name');
-  panel.appendChild(bName)
+    const bName = document.createElement("p");
+    bName.setAttribute('class', 'beer-name');
+    panel.appendChild(bName)
 
-  const bDescr = document.createElement("div");
-  bDescr.setAttribute('class', 'beer-description');
-  panel.appendChild(bDescr)
+    const bDescr = document.createElement("div");
+    bDescr.setAttribute('class', 'beer-description');
+    panel.appendChild(bDescr)
 
-  //table one
-  const table1 = document.createElement("table");
-  table1.setAttribute('class', 'beer-table1');
-  const tr1 = document.createElement('tr');
-  const tr2 = document.createElement('tr');
-  table1.appendChild(tr1)
-  table1.appendChild(tr2)
-  const th1 = document.createElement('th');
-  const th2 = document.createElement('th');
-  const div1 = document.createElement('div');
-  div1.setAttribute('class', 'property');
-  div1.textContent = "TIPO"
-  th1.appendChild(div1);
-  const div2 = document.createElement('div');
-  div2.setAttribute('class', 'property');
-  div2.textContent = "GUSTO"
-  th2.appendChild(div2);
-  tr1.appendChild(th1);
-  tr1.appendChild(th2);
-  const td1 = document.createElement('td');
-  const td2 = document.createElement('td');
-  const btype = document.createElement("p");
-  btype.setAttribute('class', 'typo');
-  td1.appendChild(btype);
-  const btaste = document.createElement("p");
-  btaste.setAttribute('class', 'taste');
-  td2.appendChild(btaste);
-  tr2.appendChild(td1);
-  tr2.appendChild(td2);
+    //table one
+    const table1 = document.createElement("table");
+    table1.setAttribute('class', 'beer-table1');
+    const tr1 = document.createElement('tr');
+    const tr2 = document.createElement('tr');
+    table1.appendChild(tr1)
+    table1.appendChild(tr2)
+    const th1 = document.createElement('th');
+    const th2 = document.createElement('th');
+    const div1 = document.createElement('div');
+    div1.setAttribute('class', 'property');
+    div1.textContent = "TIPO"
+    th1.appendChild(div1);
+    const div2 = document.createElement('div');
+    div2.setAttribute('class', 'property');
+    div2.textContent = "GUSTO"
+    th2.appendChild(div2);
+    tr1.appendChild(th1);
+    tr1.appendChild(th2);
+    const td1 = document.createElement('td');
+    const td2 = document.createElement('td');
+    const btype = document.createElement("p");
+    btype.setAttribute('class', 'typo');
+    td1.appendChild(btype);
+    const btaste = document.createElement("p");
+    btaste.setAttribute('class', 'taste');
+    td2.appendChild(btaste);
+    tr2.appendChild(td1);
+    tr2.appendChild(td2);
 
-  //table two
-  const table2 = document.createElement("table");
-  table2.setAttribute('class', 'beer-table2');
-  const tr3 = document.createElement('tr');
-  const tr4 = document.createElement('tr');
-  table2.appendChild(tr3)
-  table2.appendChild(tr4)
-  const th3 = document.createElement('th');
-  const th4 = document.createElement('th');
-  const div3 = document.createElement('div');
-  div3.setAttribute('class', 'property');
-  div3.textContent = "COLORE"
-  th3.appendChild(div3);
-  const div4 = document.createElement('div');
-  div4.setAttribute('class', 'property');
-  div4.textContent = "GRADI"
-  th4.appendChild(div4);
-  tr3.appendChild(th3);
-  tr3.appendChild(th4);
-  const td3 = document.createElement('td');
-  const td4 = document.createElement('td');
-  const bcolor = document.createElement("p");
-  bcolor.setAttribute('class', 'color');
-  td3.appendChild(bcolor);
-  const bgrad = document.createElement("p");
-  bgrad.setAttribute('class', 'grad');
-  td4.appendChild(bgrad);
-  tr4.appendChild(td3);
-  tr4.appendChild(td4);
+    //table two
+    const table2 = document.createElement("table");
+    table2.setAttribute('class', 'beer-table2');
+    const tr3 = document.createElement('tr');
+    const tr4 = document.createElement('tr');
+    table2.appendChild(tr3)
+    table2.appendChild(tr4)
+    const th3 = document.createElement('th');
+    const th4 = document.createElement('th');
+    const div3 = document.createElement('div');
+    div3.setAttribute('class', 'property');
+    div3.textContent = "COLORE"
+    th3.appendChild(div3);
+    const div4 = document.createElement('div');
+    div4.setAttribute('class', 'property');
+    div4.textContent = "GRADI"
+    th4.appendChild(div4);
+    tr3.appendChild(th3);
+    tr3.appendChild(th4);
+    const td3 = document.createElement('td');
+    const td4 = document.createElement('td');
+    const bcolor = document.createElement("p");
+    bcolor.setAttribute('class', 'color');
+    td3.appendChild(bcolor);
+    const bgrad = document.createElement("p");
+    bgrad.setAttribute('class', 'grad');
+    td4.appendChild(bgrad);
+    tr4.appendChild(td3);
+    tr4.appendChild(td4);
 
-  bDescr.appendChild(table1);
-  bDescr.appendChild(table2);
-  
-  //fill modal
+    bDescr.appendChild(table1);
+    bDescr.appendChild(table2);
+    
+    //fill modal
 
-  bImg.style.backgroundImage = birre[index].immagine
-  bName.textContent = birre[index].nome
-  btype.textContent = birre[index].tipo
-  btaste.textContent = birre[index].gusto
-  bcolor.textContent = birre[index].colore
-  bgrad.textContent = birre[index].gradazione
+    bImg.style.backgroundImage = birre[index].immagine
+    bName.textContent = birre[index].nome
+    btype.textContent = birre[index].tipo
+    btaste.textContent = birre[index].gusto
+    bcolor.textContent = birre[index].colore
+    bgrad.textContent = birre[index].gradazione
 
-  // close modal
+    // close modal
 
-  close.addEventListener("click", function() {
-  yepA.classList.remove("btOn")
-  yepB.classList.remove("btOn")
-  yepC.classList.remove("btOn")
-  yepD.classList.remove("btOn")
-  nopA.classList.remove("btOn")
-  nopB.classList.remove("btOn")
-  nopC.classList.remove("btOn")
-  nopD.classList.remove("btOn")
-  arr1[0] = null
-  arr1[1] = null
-  arr1[2] = null
-  arr1[3] = null
-  panel.remove()
-  blanket.style.display="none";
-  window.scrollTo({
-    top: 720,
-    left: 0,
-    behavior: "smooth"
+    close.addEventListener("click", function() {
+    yepA.classList.remove("btOn")
+    yepB.classList.remove("btOn")
+    yepC.classList.remove("btOn")
+    yepD.classList.remove("btOn")
+    nopA.classList.remove("btOn")
+    nopB.classList.remove("btOn")
+    nopC.classList.remove("btOn")
+    nopD.classList.remove("btOn")
+    arr1[0] = null
+    arr1[1] = null
+    arr1[2] = null
+    arr1[3] = null
+    panel.remove()
+    blanket.style.display="none";
+    window.scrollTo({
+      top: 720,
+      left: 0,
+      behavior: "smooth"
+    });
   });
-});
+  } catch(e) {
+    alert('createPanel' + e)
+  }
 }
 
 // button control
@@ -330,20 +330,28 @@ const buttonY = [yepA, yepB, yepC, yepD]
 const buttonN = [nopA, nopB, nopC, nopD]
 
 function answerY(x,y) {
+  try{
     for (i=0; i<(x.length); i++) {
         if (x[i].classList.contains("btOn")) {
             y[i] = true;
         }
     }
+  } catch(e){
+    alert('answerY' + e)
+  }
     console.log(y)
 }
 
 function answerN(x,y) {
+  try{
     for (i=0; i<(x.length); i++) {
         if (x[i].classList.contains("btOn")) {
             y[i] = false;
         }
     }
+  } catch(e){
+    alert('answerN' + e)
+  }
     console.log(y)
 }
 
@@ -352,7 +360,8 @@ function answerN(x,y) {
 
 function insert(arr1) {
 
-  if (arr1[0] == true && arr1[1] == true && arr1[2] == true && arr1[3] == true) { 
+  try{
+    if (arr1[0] == true && arr1[1] == true && arr1[2] == true && arr1[3] == true) { 
         createPanel(0)
       } else if (arr1[0] == true && arr1[1] == true && arr1[2] == true && arr1[3] == false) { 
         createPanel(1)
@@ -388,6 +397,9 @@ function insert(arr1) {
         createPanel(16)
         //bName.style.fontSize = "1.2em";
       }
+  } catch(e) {
+    alert('insert' + e)
+  }
 }
 
 const result = document.querySelector(".result");
